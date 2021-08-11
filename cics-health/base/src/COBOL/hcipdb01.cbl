46c67c3ca72bf313a4a6ca79733f065f455a3b97
RegiBrazil <rbarosa@us.ibm.com>
2021-08-11 18:05:15 -0400

    Fixed Jenkins and       * Changed HCIPDB01 August 11 2021 -    18:04

--- cics-health/base/src/COBOL/hcipdb01.cbl
+++ cics-health/base/src/COBOL/hcipdb01.cbl
@@ -1,7 +1,7 @@
       *****************************************************************
       * Used on CICS trx HCAZ
       * invoked hen selecting option 2 - Inquire Patient
-      * Changed HCIPDB01 August 11 2021 -    17:41
+      * Changed HCIPDB01 August 11 2021 -    18:04
       *  Look for %bug to introduce bugs
       ****************************************************************
        IDENTIFICATION DIVISION.
