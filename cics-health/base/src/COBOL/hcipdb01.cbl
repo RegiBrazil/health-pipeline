ee8049b52910f3ee0738fa91fcecafbeb99b28de
RegiBrazil <rbarosa@us.ibm.com>
2021-08-26 12:01:28 -0400

    Incremental build - Chnaged HCIPDB01

--- cics-health/base/src/COBOL/hcipdb01.cbl
+++ cics-health/base/src/COBOL/hcipdb01.cbl
@@ -1,7 +1,7 @@
       *****************************************************************
       * Used on CICS trx HCAZ
       * invoked hen selecting option 2 - Inquire Patient
-      * Changed HCIPDB01 August 25 2021 -    16:47
+      * Changed HCIPDB01 August 26 2021 -    12:00
       *  Look for %bug to introduce bugs
       ****************************************************************
        IDENTIFICATION DIVISION.
