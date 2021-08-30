f18456f396e6856df10df332a201cf4a94739fce
RegiBrazil <rbarosa@us.ibm.com>
2021-08-30 14:56:26 -0400

    chnaged jenksfile added -d at build for CC

--- cics-health/base/src/COBOL/hcipdb01.cbl
+++ cics-health/base/src/COBOL/hcipdb01.cbl
@@ -1,7 +1,7 @@
       *****************************************************************
       * Used on CICS trx HCAZ
       * invoked hen selecting option 2 - Inquire Patient
-      * Changed HCIPDB01 August 30 2021 -    14:35
+      * Changed HCIPDB01 August 30 2021 -    14:56
       *  Look for %bug to introduce bugs
       ****************************************************************
        IDENTIFICATION DIVISION.
