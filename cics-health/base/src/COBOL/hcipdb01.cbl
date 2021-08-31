1f1c67f74900f5ef445a3229d0e81c56001a3fb8
RegiBrazil <rbarosa@us.ibm.com>
2021-08-31 11:30:23 -0400

    * Changed HCIPDB01 August 31 2021 -    10:01

--- cics-health/base/src/COBOL/hcipdb01.cbl
+++ cics-health/base/src/COBOL/hcipdb01.cbl
@@ -1,7 +1,7 @@
       *****************************************************************
       * Used on CICS trx HCAZ
       * invoked hen selecting option 2 - Inquire Patient
-      * Changed HCIPDB01 August 30 2021 -    19:53
+      * Changed HCIPDB01 August 31 2021 -    10:01
       *  Look for %bug to introduce bugs
       ****************************************************************
        IDENTIFICATION DIVISION.
