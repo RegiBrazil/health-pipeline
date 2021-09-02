cb8d2f9a10f25faeed6f21fa37f2a2d6bd35849c
RegiBrazil <rbarosa@us.ibm.com>
2021-09-02 15:13:15 -0400

    Port now 8006 and output at /opt/cc/output

--- cics-health/base/src/COBOL/hcipdb01.cbl
+++ cics-health/base/src/COBOL/hcipdb01.cbl
@@ -1,7 +1,7 @@
       *****************************************************************
       * Used on CICS trx HCAZ
       * invoked hen selecting option 2 - Inquire Patient
-      * Changed HCIPDB01 September 02 2021 -    12:53
+      * Changed HCIPDB01 September 02 2021 -    15:12
       *  Look for %bug to introduce bugs
       ****************************************************************
        IDENTIFICATION DIVISION.
